965a7709eb9a79905ed5cf68654f864f9dfe0eec
Halim Can Başkaya <cnbskya@gmail.com>
2020-12-23 10:54:54 +0300

    Last Push

---
+++ Assets/TenMinutesScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class TenMinutesScore : MonoBehaviour
+{
+
+    public Text scoreText;
+    public Hexagon hexagon;
+   
+
+    void Update()
+    {
+        if(hexagon.transform.localScale.x <= .05f)
+        {
+            scoreText.text += hexagon.transform.position.x.ToString("0");
+        }
+        
+    }
+}
